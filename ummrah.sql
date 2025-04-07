97238670ba12b5ece1ed46ee92f437b119c35e38
Usama Bashir <usamab@mudsoft.tech>
2025-04-07 21:12:18 +0500

    first commit

--- ummrah.sql
+++ ummrah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 07, 2025 at 10:51 AM
+-- Generation Time: Apr 07, 2025 at 04:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.3.14
 
